8d6df8cd40ce77d7b6603885f47c7c47ae16839a
OURAMDANE <32832470+OURAMDANE@users.noreply.github.com>
2017-11-13 12:50:34 +0100

    Add files via upload

---
+++ tp3/form-jquery-validation.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+   // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
+   // voir plus : https://www.w3schools.com/js/js_htmldom.asp
+    console.log( "DOM ready!" );
+    
+    // Y mettre le code jQuery pour valider tous les champs du formulaire
+    $("#submit").on("click",function(event){
+	  event.preventDefault();
+	  console.log( "click" );
+	  document.querySelector(".modal-title").textContent = "Message Modal"
+   $('#myModal').modal("show");
+
+	});
+});
